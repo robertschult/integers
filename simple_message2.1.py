a640043e126c7785d4565958f96ea0117e957aad
robertschult <48728030+robertschult@users.noreply.github.com>
2020-05-05 23:30:11 -0400

    Create simple_message2.1.py

---
+++ simple_message2.1.py
@@ -0,0 +1,14 @@
+name = "robert"
+print(name)
+
+alien_sound = "pew pew pew"
+print(alien_sound)
+
+narrator = "aliens zooming bye"
+print(narrator)
+
+bread = "yeast, water, sugar, flour, baking powder, salt"
+print(bread)
+
+rice_crispy_treats = "1 stick butter,1 bag marshmallow, 5 cups rice crispy's"
+print(rice_crispy_treats)

b3427174fa0d104112ea2b47d5443866bce2b98b
robertschult <48728030+robertschult@users.noreply.github.com>
2020-05-08 12:50:34 -0400

    Create Squares.4.6.py

---
+++ Squares.4.6.py
@@ -0,0 +1,12 @@
+sex = list(range(2, 21, 2))
+print(sex)
+numbers = list(range(1, 21, 2))
+print(numbers)
+for numero in range(1, 21, 3):
+    print(numero)
+cubes = []
+for shit in range(1, 10):
+    cube = value ** 3
+    cubes.append(cube)
+    
+print(cubes)
